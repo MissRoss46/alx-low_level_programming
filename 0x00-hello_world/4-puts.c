32af70326267712d202de054340afbe039f9df7c
Jacqueline Ross <jacqueokaka46@gmail.com>
2022-10-14 16:47:25 -0700

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - entry point
@@ -11,13 +9,9 @@
  *    * Return: Always 0 (success)
  *
  *     */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
-
+		return (0);
 }
