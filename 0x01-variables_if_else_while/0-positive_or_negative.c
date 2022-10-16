c45c450dbc5c50861fab4e948c772cc1a3e0b1b3
Jacqueline Ross <jacqueokaka46@gmail.com>
2022-10-16 16:01:21 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
@@ -6,14 +6,14 @@
  * Description: Get a random number and print the number
  * and if it is positive, negative, or zero
  * Return: 0
- */
+ *  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n >  0)
+	if (n > 0)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
