8d090381ce43a2d51f06e1edf1ce3db1771cb3ee
Jacqueline Ross <jacqueokaka46@gmail.com>
2022-10-16 16:29:50 -0700

    Alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,20 @@
 int main(void)
 {
 	char alpha = 'a';
+
 	while (alpha <= 'z')
 {
 	putchar(alpha);
 	alpha++;
 }
 alpha = 'A';
+
 while (alpha <= 'Z')
 {
 	putchar(alpha);
 	alpha++;
 }
 putchar('\n');
+
 return (0);
 }
