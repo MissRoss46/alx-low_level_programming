0b644c549d4b993000c0d382e03f7fee93ceab8a
Jacqueline Ross <jacqueokaka46@gmail.com>
2022-10-19 19:02:43 -0700

    Clearing all remaining tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - adds two integrers and returns the result
+ * @x: number being added
+ *  @y: number being added
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
